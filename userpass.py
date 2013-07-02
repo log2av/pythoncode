7e96e29de22da2c8a4140ba0183c7793bdbd187e
Abhishek Verma <abhishek.verma@laitkor.com>
2013-07-02 16:57:16 +0530

    Final version of automatic change of password and sending on mail according to crontab

--- userpass.py
+++ userpass.py
@@ -1,8 +1,12 @@
+#!/root/python/Python-3.3.0/python 
 import string
 import os
 import random
 import string
-user1=input('Enter the name of the user ')
+import smtplib
+
+#user1=input('Enter the name of the user ')
+user1='darklord'
 pass1= ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(10))
 cmd='echo -e "' + pass1 + '\n' + pass1 + '" | passwd ' + user1
 if os.system(cmd)!= 0:
@@ -10,5 +14,33 @@ if os.system(cmd)!= 0:
 else:
     print('New password for user ' + user1 + ' is ' + pass1 )
 
+#SMTP_SERVER = 'smtp.gmail.com'
+#SMTP_PORT = 587
+server = 'smtp.gmail.com'
+port = 587
 
-
+sender = 'log2av@gmail.com'
+recipient = 'abhishek.verma@laitkor.com'
+subject = 'Change of Password'
+body = 'New Password for user ' + user1 + ' is ' + pass1 
+ 
+"Sends an e-mail to the specified recipient."
+ 
+body = "" + body + ""
+ 
+headers = ["From: " + sender,
+           "Subject: " + subject,
+           "To: " + recipient,
+           "MIME-Version: 1.0",
+           "Content-Type: text/html"]
+headers = "\r\n".join(headers)
+ 
+session = smtplib.SMTP(server, port)
+ 
+session.ehlo()
+session.starttls()
+session.ehlo
+session.login(sender, 'mail..25846+')
+ 
+session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+session.quit()
