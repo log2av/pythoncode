c97ff49712c5ffb355dc1124c4455a72d57ce088
log2av <log2av@gmail.com>
2013-09-30 16:00:43 +0530

    Mysql backup and sending to a remote server

---
+++ mysqlbackup.py
@@ -0,0 +1,21 @@
+#!/root/python/Python-3.3.0/python
+
+import os
+import time
+import datetime
+date1 = time.strftime('%Y%m%d%H%M%S')
+f = open('/root/mypython/dblist.txt') # this files contains the name of databases
+line = f.readline()
+f.close()
+words = line.split()
+for word in words:
+    cmd = "mysqldump -u root -pabhishek {0} > {0}_{1}.sql".format(word, date1) # takes backup in the same location as script
+    cmd2 = "zip {0}_{1}.zip {0}_{1}.sql".format(word, date1) # zips the backup just taken
+    cmd3 = "rm -f {0}_{1}.sql".format(word, date1) # deletes the .sql backup just taken. after this step only .zip backup remains. remove this line if u need both .sql and .zip
+    cmd4 = " scp {0}_{1}.zip root@192.168.1.105:/home/laitkor/dev_test ".format(word, date1)
+    os.system(cmd)
+    os.system(cmd2)
+    os.system(cmd3)
+    if os.system(cmd4)== 0:
+        result = "Backup Successful"
+print(result)
