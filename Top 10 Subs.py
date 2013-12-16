18c5c8925cfd66ea44b7ac24f0d9da271b2a0d2d
abhishek <log2av@gmail.com>
2013-12-16 11:53:18 +0530

    Manual coomands

---
+++ Top 10 Subs.py
@@ -0,0 +1,13 @@
+import subprocess
+import fileinput
+import sys
+
+date = raw_input('Enter the date in format YYYYMMDD ')
+open('top10.txt', 'w').close()
+subprocess.call(['notepad.exe', 'top10.txt'])
+for line in fileinput.input('top10.txt', inplace=True):
+	line = line.rstrip()
+	line = 'zcat *' + date + '* | grep -a ' + line + ' > /opt/crestelsetup/report_temp/' + line + '.txt ; '
+	#line = prefix + ' ' + line + ' ' + suffix + '\n'
+	sys.stdout.write(line)
+subprocess.call(['notepad.exe', 'top10.txt'])
